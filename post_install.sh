194f3daa9329356ce58b69e1dad997a279702627
Václav Navrátil <vaclav@navratil.vn>
2019-03-10 00:05:47 +0100

    sed no longer sad

--- post_install.sh
+++ post_install.sh
@@ -35,24 +35,24 @@ sudo -u www wp core install --url=wordpress.local --title=iocage-wp-plugin --adm
 
 cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
 
-/sed -i "" 's%;open_basedir =%open_basedir = /usr/local/www/wp:/tmp%g' /usr/local/etc/php.ini
-/sed -i "" 's/memory_limit = 128M/memory_limit = 256M/g' /usr/local/etc/php.ini
-/sed -i "" 's/post_max_size = 8M/post_max_size = 32M/g' /usr/local/etc/php.ini
-/sed -i "" 's/upload_max_filesize = 2M/upload_max_filesize = 32M/g' /usr/local/etc/php.ini
-/sed -i "" 's/max_execution_time = 30/max_execution_time = 180/g' /usr/local/etc/php.ini
-/sed -i "" 's/max_input_time = 60/max_input_time = 180/g' /usr/local/etc/php.ini
+sed -i "" 's%;open_basedir =%open_basedir = /usr/local/www/wp:/tmp%g' /usr/local/etc/php.ini
+sed -i "" 's/memory_limit = 128M/memory_limit = 256M/g' /usr/local/etc/php.ini
+sed -i "" 's/post_max_size = 8M/post_max_size = 32M/g' /usr/local/etc/php.ini
+sed -i "" 's/upload_max_filesize = 2M/upload_max_filesize = 32M/g' /usr/local/etc/php.ini
+sed -i "" 's/max_execution_time = 30/max_execution_time = 180/g' /usr/local/etc/php.ini
+sed -i "" 's/max_input_time = 60/max_input_time = 180/g' /usr/local/etc/php.ini
 
-/sed -i "" 's%listen = 127.0.0.1:9000%listen = 127.0.0.1:9000%g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/;listen.owner = www/listen.owner = www/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/;listen.group = www/listen.group = www/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/;listen.mode = 0660/listen.mode = 0660/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/pm = dynamic/pm = ondemand/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/pm.max_children = 5/pm.max_children = 50/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/;pm.process_idle_timeout = 10s;/pm.process_idle_timeout = 30s/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/;pm.max_requests = 500/pm.max_requests = 500/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/;security.limit_extensions = .php .php3 .php4 .php5 .php7/security.limit_extensions = .php .php3 .php4 .php5 .php7/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/;php_flag[display_errors] = off/php_flag[display_errors] = off/g' /usr/local/etc/php-fpm.d/www.conf
-/sed -i "" 's/;php_admin_flag[log_errors] = on/php_admin_flag[log_errors] = on/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's%listen = 127.0.0.1:9000%listen = /var/run/php-fpm.sock%g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/;listen.owner = www/listen.owner = www/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/;listen.group = www/listen.group = www/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/;listen.mode = 0660/listen.mode = 0660/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/pm = dynamic/pm = ondemand/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/pm.max_children = 5/pm.max_children = 50/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/;pm.process_idle_timeout = 10s;/pm.process_idle_timeout = 30s/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/;pm.max_requests = 500/pm.max_requests = 500/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/;security.limit_extensions = .php .php3 .php4 .php5 .php7/security.limit_extensions = .php .php3 .php4 .php5 .php7/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/;php_flag[display_errors] = off/php_flag[display_errors] = off/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i "" 's/;php_admin_flag[log_errors] = on/php_admin_flag[log_errors] = on/g' /usr/local/etc/php-fpm.d/www.conf
 
 service php-fpm start
 
