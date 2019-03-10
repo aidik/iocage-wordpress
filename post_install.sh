62bf2e0e9e72e2d15de1d688ac7c857fcf2256f1
Václav Navrátil <vaclav@navratil.vn>
2019-03-10 15:31:26 +0100

    spit it

--- post_install.sh
+++ post_install.sh
@@ -2,13 +2,13 @@
 
 set dbadmin="`openssl rand -base64 12`"
 echo "DB Root Password" > /root/PLUGIN_INFO
-echo $dbadmin >> /root/PLUGIN_INFO
+echo '$dbadmin' >> /root/PLUGIN_INFO
 set wpdbuser="`openssl rand -base64 12`"
 echo "WPDB User Password" >> /root/PLUGIN_INFO
-echo $wpdbuser >> /root/PLUGIN_INFO
+echo '$wpdbuser' >> /root/PLUGIN_INFO
 set wpadmin="`openssl rand -base64 12`"
 echo "WP Admin Password" >> /root/PLUGIN_INFO
-echo $wpadmin >> /root/PLUGIN_INFO
+echo '$wpadmin' >> /root/PLUGIN_INFO
 
 sysrc -f /etc/rc.conf mysql_enable="YES"
 sysrc -f /etc/rc.conf nginx_enable="YES"
@@ -30,7 +30,7 @@ mkdir /usr/local/www/wp
 cd /usr/local/www/wp
 chown -R www:wheel /usr/local/www/wp/
 sudo -u www wp core download
-sudo -u www wp config create --dbname=wpdb --dbuser=wpdbuser --dbpass=$wpdbuser
+sudo -u www wp config create --dbname=wpdb --dbuser=wpdbuser --dbpass='$wpdbuser'
 sudo -u www wp core install --url=wordpress.local --title=iocage-wp-plugin --admin_user=wpadmin --admin_password=$wpadmin --admin_email=no@email.sry
 
 cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
